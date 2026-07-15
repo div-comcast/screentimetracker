9529a951184b1aabf8ee0f593b4893bfe56ee8ac
div-comcast <Divyanshu_Divyanshu@comcast.com>
2026-07-15 21:47:06 +0530

    intoduced date_rangebar

--- lib/frontend/screens/dashboard.dart
+++ lib/frontend/screens/dashboard.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import '../theme/app_theme.dart';
 import '../widgets/header.dart';
 import '../widgets/calendar.dart';
+import '../widgets/date_rangebar.dart';
 
 class DashboardScreen extends StatefulWidget {
   const DashboardScreen({super.key});
@@ -11,8 +12,20 @@ class DashboardScreen extends StatefulWidget {
 }
 
 class _DashboardScreenState extends State<DashboardScreen> {
-  DateTime? _selectedStart;
-  DateTime? _selectedEnd;
+  late DateTime _selectedStart;
+  late DateTime _selectedEnd;
+  late final DateTime _minDate;
+  late final DateTime _maxDate;
+
+  @override
+  void initState() {
+    super.initState();
+    final today = DateTime.now();
+    _maxDate = DateTime(today.year, today.month, today.day);
+    _minDate = _maxDate.subtract(const Duration(days: 20));
+    _selectedStart = _maxDate;
+    _selectedEnd = _maxDate;
+  }
 
   void _openCalendar() {
     showCalendarSheet(
@@ -20,11 +33,34 @@ class _DashboardScreenState extends State<DashboardScreen> {
       onApply: (start, end) {
         setState(() {
           _selectedStart = start;
-          _selectedEnd = end;
+          _selectedEnd = end ?? start;
         });
       },
     );
   }
+
+  void _shiftBack() {
+    final span = _selectedEnd.difference(_selectedStart).inDays;
+    final newStart = _selectedStart.subtract(const Duration(days: 1));
+    if (newStart.isBefore(_minDate)) return;
+    setState(() {
+      _selectedStart = newStart;
+      _selectedEnd = newStart.add(Duration(days: span));
+    });
+  }
+
+  void _shiftForward() {
+    final span = _selectedEnd.difference(_selectedStart).inDays;
+    final newEnd = _selectedEnd.add(const Duration(days: 1));
+    if (newEnd.isAfter(_maxDate)) return;
+    setState(() {
+      _selectedEnd = newEnd;
+      _selectedStart = newEnd.subtract(Duration(days: span));
+    });
+  }
+
+  bool get _canGoBack => _selectedStart.isAfter(_minDate);
+  bool get _canGoForward => _selectedEnd.isBefore(_maxDate);
 
   @override
   Widget build(BuildContext context) {
@@ -35,6 +71,13 @@ class _DashboardScreenState extends State<DashboardScreen> {
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             ScreenTimeHeader(onCalendarTap: _openCalendar),
+            DateBar(
+              start: _selectedStart,
+              end: _selectedStart == _selectedEnd ? null : _selectedEnd,
+              onTap: _openCalendar,
+              onBack: _canGoBack ? _shiftBack : null,
+              onForward: _canGoForward ? _shiftForward : null,
+            ),
           ],
         ),
       ),
